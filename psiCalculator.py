e5f7a6d594988d6c555b08ea5eafbd21bd4b39ae
Juan_Luis_Trincado <juanluis.trincado@upf.edu>
2017-08-18 13:38:57 +0200

    Added save_tpm_events to psiCalculator.py

--- psiCalculator.py
+++ psiCalculator.py
@@ -8,6 +8,7 @@ Created on Tue May  6 12:19:45 2014
 
 import sys
 import logging
+import numpy as np
 from argparse import ArgumentParser, RawTextHelpFormatter
 from lib.tools import *
 
@@ -26,11 +27,16 @@ parser.add_argument("-o", "--output-file", required=True,
                     help="Output psi file.")
 parser.add_argument("-f", "--total-filter", type=float, default=0,
                     help="Minimum total expression of the transcripts involved in the event.")
+parser.add_argument('-s', '--save_tpm_events',
+                    action="store_true",
+                    default=False,
+                    help="Boolean. If True, save the TPM of the events in an external file (Default: False).")
 parser.add_argument("-m", "--mode", default="INFO",
                     help="to choose from DEBUG, INFO, WARNING, ERROR and CRITICAL")
 
 
 def main():
+
     args = parser.parse_args()
 
     #Parsing arguments
@@ -49,6 +55,7 @@ def main():
 
     expression_dictionary = {}  # to store transcript expression [transcript_id] = {[colId] = expression}
     psi_dictionary = {}  # to store the psi values calculated [event_id] = {[colId] = expression}
+    expression_dictionary_events = {}  # to store the expression values calculated [event_id] = {[colId] = expression}
     col_ids = []  # to store all the column_id for the expression fields
     try:
 
@@ -79,6 +86,7 @@ def main():
             #Buffering IOE and calculating PSI
             factory = FactoryReader()
             r = factory.getReader("ioe")
+            # r.openFile(args.ioe_file)
             r.openFile(args.ioe_file)
             logger.info("Calculating PSI from the ioe file.") 
             line = r.readLine()
@@ -93,10 +101,12 @@ def main():
                         continue
                     skip = False  # to avoid checking total_iso if event == "NA"
                     psi_dictionary[arguments["event_id"]] = {}
+                    expression_dictionary_events[arguments["event_id"]] = {}
                     for x in col_ids:
                         #set to 0 all the cumulative expression values
                         alternative_transcripts[x] = 0
                         total_transcripts[x] = 0
+                    #Add all the expression values of the alternative transcripts
                     for tr in arguments["alt_iso"].rstrip("\n").split(","):
                         try:
                             for x in col_ids:
@@ -110,7 +120,9 @@ def main():
                                          arguments["event_id"])
                             for x in col_ids:
                                 psi_dictionary[arguments["event_id"]][x] = "NA"
+                                expression_dictionary_events[arguments["event_id"]][x] = "NA"
                             skip = True
+                    # Add all the expression values of the total transcripts
                     if not skip:
                         for tr in arguments["total_iso"].rstrip("\n").split(","):
                             try:
@@ -125,6 +137,7 @@ def main():
                                              arguments["event_id"])
                                 for x in col_ids:
                                     psi_dictionary[arguments["event_id"]][x] = "NA"
+                                    expression_dictionary_events[arguments["event_id"]][x] = "NA"
                                 skip = True
                     for x in col_ids:
                         #If it passes the filter and skip == False
@@ -132,13 +145,18 @@ def main():
                             try:
                                 psi_dictionary[arguments["event_id"]][x] = (
                                     alternative_transcripts[x]/total_transcripts[x])
+                                #Here can we introduce the calculation of the mean of the total transcripts for each event
+                                expression_dictionary_events[arguments["event_id"]][x] = np.mean(total_transcripts[x])
                             except ZeroDivisionError:
                                 logger.debug("Zero division for event %s.(psi= -1)." % arguments["event_id"])
                                 psi_dictionary[arguments["event_id"]][x] = -1
+                                expression_dictionary_events[arguments["event_id"]][x] = np.mean(total_transcripts[x])
                         #If it passes the filter but skip == True
                         elif not skip:   
                             #for x in col_ids:
-                                psi_dictionary[arguments["event_id"]][x] = -1
+                            psi_dictionary[arguments["event_id"]][x] = -1
+                            expression_dictionary_events[arguments["event_id"]][x] = np.mean(total_transcripts[x])
+
             except StopIteration:
                 writer = Writer.getWriter("PSI")
                 logger.info("Generating output %s" % (output_file + ".psi"))
@@ -149,6 +167,19 @@ def main():
                     psi_line = PsiWriter.lineGenerator(key, value, col_ids)
                     writer.writeLine("\t".join(psi_line))
                 writer.closeFile()
+
+                if (args.save_tpm_events == True):
+                    #Save the expression of the events
+                    writer = Writer.getWriter("TPM")
+                    logger.info("Generating output %s" % (output_file + ".tpm"))
+                    writer.openFile(output_file)
+                    writer.writeLine("\t".join(col_ids), False)
+                    for key, value in sorted(expression_dictionary_events.items()):
+                        logger.debug("Calculating tpm for %s" % key)
+                        psi_line = PsiWriter.lineGenerator(key, value, col_ids)
+                        writer.writeLine("\t".join(psi_line))
+                    writer.closeFile()
+
     except BaseException:
         logger.error("Unknown error: %s" % sys.exc_info()[1].args[0])
         sys.exit(1)
